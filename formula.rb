c6fa33769619bf3d0a8e9dafc0a6efb8f217419b
utsmannn <kucingapes@gmail.com>
2020-09-20 16:29:22 +0700

    fix generate app id

--- formula.rb
+++ formula.rb
@@ -1,10 +1,10 @@
 class Sepack < Formula
 	desc "Command-line interface for build android project base on MVVM"
 	homepage "https://github.com/utsmannn/android-sepack"
-	url "https://github.com/utsmannn/android-sepack/archive/0.1.1.tar.gz"
+	url "https://github.com/utsmannn/android-sepack/archive/0.1.2.tar.gz"
 	sha256 "816cf4e38aa97935fbb9efdc0c36e516b40f677cf3f329acab355cf9286fe6f4"
 	head "https://github.com/utsmannn/android-sepack.git"
-	version "0.1.1"
+	version "0.1.2"
 
 	def install
 		bin.install "sepack"
