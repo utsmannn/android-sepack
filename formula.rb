c8217cdafed340eb3b3ed2c29c2f5c4e55fff741
utsmannn <kucingapes@gmail.com>
2020-09-20 07:09:32 +0700

    update formula

--- formula.rb
+++ formula.rb
@@ -4,6 +4,7 @@ class Sepack < Formula
 	url "https://github.com/utsmannn/android-sepack/archive/0.1.1.tar.gz"
 	sha256 "816cf4e38aa97935fbb9efdc0c36e516b40f677cf3f329acab355cf9286fe6f4"
 	head "https://github.com/utsmannn/android-sepack.git"
+	version "0.1.1"
 
 	def install
 		bin.install "android-sepack"
