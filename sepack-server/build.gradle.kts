7fda38eeb8fc4d71ac16f195d98d1221a82bb78e
utsmannn <kucingapes@gmail.com>
2021-05-10 04:30:41 +0700

    fix shadow jar

--- sepack-server/build.gradle.kts
+++ sepack-server/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.github.johnrengelman.shadow") version "6.1.0"
 	kotlin("jvm") version "1.4.32"
 	kotlin("plugin.spring") version "1.4.32"
 }
@@ -32,4 +33,12 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+	manifest {
+		attributes(mapOf(
+			"Main-Class" to "com.utsman.sepackserver.SepackServerApplicationKt"
+		))
+	}
 }
